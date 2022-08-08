2d4146998905fb8a820cb7fe84161e26e74299fc
Ali Sherifi <ali.sherifi@live.com>
2022-08-08 21:07:43 +0200

    Add button that promts to user how many squares it wants Clear initial sketch and append new boxes

--- index.js
+++ index.js
@@ -1,23 +1,56 @@
 const container = document.querySelector('.container');
 
 
+/* Starting sketch with 16 rows and 16 columns */
+for (let i = 0; i < 256; i++) {
+    let div = document.createElement('div');
 
-
-for(let i = 0; i < 256; i++) {
-    const div = document.createElement('div');
-    
     div.classList.add('boxes');
 
     container.appendChild(div);
 
-/*     const newLine = document.createElement('br');
-
-    newLine.classList.add('brr');
-
-    if(i % 17 === 0) {
-        container.appendChild(newLine)
-    } else {
-      container.appendChild(div);  
-    } */
-    
 }
+
+/* Change boxes background-color after hovering */
+const boxes = document.querySelectorAll('.boxes');
+
+boxes.forEach(box => {
+    box.addEventListener('mouseover', () => {
+        box.style.backgroundColor = 'red';
+    });
+});
+
+
+/* Change sketch after user input */
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', getSquares);
+
+function getSquares () {
+   let squares = prompt('How many squares on each side?');
+   newSketch(squares);
+}
+
+function newSketch (num) {
+    container.textContent = '';
+
+    let totalSquares = num * num;
+
+    let side = parseInt(num) + 1; //the input is a string and to get correct squares on each side plus 1(?)
+
+
+for (let i = 0; i < totalSquares; i++) {
+    let div = document.createElement('div');
+
+    div.classList.add('boxes');
+    div.style.setProperty('width', 'calc(100% * (1/' + side + '))');
+    
+
+    container.appendChild(div);
+
+}
+
+}
+
+
+/* NEED TO CHANGE COLOR AFTER HOVERING NEW BOXES */
