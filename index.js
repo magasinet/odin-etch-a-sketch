584bd4619a9365d62a9264035ca3bf4d4b47b476
Ali Sherifi <ali.sherifi@live.com>
2022-08-09 14:03:22 +0200

    Mutation Observer Add mutation observer event to change background color when nodelist is changes. Make sketch funcional after user input

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const container = document.querySelector('.container');
 
 
-/* Starting sketch with 16 rows and 16 columns */
+/* 
+Mutation Observer
+Change background color on mouseover when element nodelist is changed via DOM.
+Adding this changes the color at initial sketch and when the user changes the sketch.
+*/
+let observer = new MutationObserver(() => {
+    // what to do when DOM changes (backgroundColor = red)
+    let boxes = document.querySelectorAll('.boxes');
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.style.backgroundColor = 'red';
+        });
+    });
+});
+// observes the children of id="elementToBeObserved"
+observer.observe(elementToBeObserved, {
+    childList: true,
+});
+
+
+
+
+// Starting sketch with 16 rows and 16 columns
 for (let i = 0; i < 256; i++) {
     let div = document.createElement('div');
 
@@ -11,27 +33,18 @@ for (let i = 0; i < 256; i++) {
 
 }
 
-/* Change boxes background-color after hovering */
-const boxes = document.querySelectorAll('.boxes');
 
-boxes.forEach(box => {
-    box.addEventListener('mouseover', () => {
-        box.style.backgroundColor = 'red';
-    });
-});
-
-
-/* Change sketch after user input */
+// Change sketch after user input
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', getSquares);
 
-function getSquares () {
-   let squares = prompt('How many squares on each side?');
-   newSketch(squares);
+function getSquares() {
+    let squares = prompt('How many squares on each side?');
+    newSketch(squares);
 }
 
-function newSketch (num) {
+function newSketch(num) {
     container.textContent = '';
 
     let totalSquares = num * num;
@@ -39,18 +52,15 @@ function newSketch (num) {
     let side = parseInt(num) + 1; //the input is a string and to get correct squares on each side plus 1(?)
 
 
-for (let i = 0; i < totalSquares; i++) {
-    let div = document.createElement('div');
+    for (let i = 0; i < totalSquares; i++) {
+        let div = document.createElement('div');
 
-    div.classList.add('boxes');
-    div.style.setProperty('width', 'calc(100% * (1/' + side + '))');
-    
+        div.classList.add('boxes');
+        div.style.setProperty('width', 'calc(100% * (1/' + side + '))');
 
-    container.appendChild(div);
-
-}
+        container.appendChild(div);
+    }
 
 }
 
-
-/* NEED TO CHANGE COLOR AFTER HOVERING NEW BOXES */
+/* ADD MAXIMUM OF 100 in user input */
